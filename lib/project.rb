7300c6fc636486f78e3a37e6d626de879130515b
Micah Bowie <micahbowie20@gmail.com>
2020-07-25 01:30:20 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project 
   attr_reader :title  
   
-
+@@backers = []
   def initialize(title)
     @title=title
-    backers
+    @@backers
   end
   
   def backers
-    @@backers = []
+    @@backers
   end 
   
   def add_backer(backer_name)
